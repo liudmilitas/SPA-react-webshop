0ca1105d24b4820bf334bbd8c810a81df50447f3
Celina <celina.johansson@hotmail.com>
2022-03-02 19:53:51 +0100

    localstorage
    klar med localstorage hanterare

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from './components/Header.js'
 
 
 function App() {
+  
   return (
     <div className="App">
+      <Header />
       Webshop in React
     </div>
   );
