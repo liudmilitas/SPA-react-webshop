33dcdc7b0a7aa20085d1ca5db63177c165778d15
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2022-03-02 09:01:39 +0100

    saving the header template

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <img src="#" alt="Clay Shop Logo" className='logo-image'/>
+      <h1>Clay Shop</h1>
+      <ul className='header-media-links'>
+        <li>
+          <a>
+          <img src="#" alt="Instagram" />
+          </a>
+        </li>
+        <li>
+          <a>
+          <img src="#" alt="Twitter" />
+          </a>
+        </li>
+        <li>
+          <a>
+          <img src="#" alt="Facebook"/>
+          </a>
+        </li>
+      </ul>
+      <div className="shopping-cart-wrapper">
+        <button className='shopping-cart-button'></button>
+      </div>
+    </header>
   )
 }
 
