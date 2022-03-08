9c0b7e7837345082a3903b453ffcb42c60555e6a
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2022-03-08 23:58:59 +0100

    added left constraint to the scroller

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,13 +3,20 @@ import React, { useRef, useEffect, useState } from 'react'
 import images from '../images.js'
 
 export default function Slider() {
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+
+    useEffect(()=> {
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+    }, [])
+
     return (
         <div>
-            <motion.div className='carousel'>
-                <motion.div drag="x" dragConstraints={{right: 0}} className='inner-carousel'>
+            <motion.div ref={carousel} className='carousel' whileTap={{cursor: 'grabbing'}}>
+                <motion.div drag="x" dragConstraints={{right: 0, left: -width}} className='inner-carousel'>
                     { images.map(image => {
                             return(
-                                <motion.div className='carousel-single-item'>
+                                <motion.div className='carousel-single-item' key={image}>
                                     <img src={image} alt=""/>
                                 </motion.div>
                             )
